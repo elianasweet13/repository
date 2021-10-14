a6978eb8c1fc8fe2895077a889a0a2ebe4cfdd68
elianasweet <elsw4258@colorado.edu>
2021-10-14 11:50:30 -0600

    push

--- week8/index.js
+++ week8/index.js
@@ -1,18 +1,14 @@
 //console.log (document.getElementsByClassName("options") );
 
+console.log( document.getElementsByClassName("options") );
+
+
 const images = ["moon.jpeg", "sun,jpg", "stars.jpeg"];
 const title = ["moon", "sun", "stars"];
 
 
-for(i=0;i<document.getElementsByClassName('options').length;i++){
+for(i=0;i<document.getElementsByClassName('options').length; i++){
 
   document.getElementByClassName('label')[i].innerHTML = title[i];
 
-  
-console.log(document.getElementsByClassName('options')[i].innerHTML);
-document.getElementsByClassName("options")[i].innerHTML += '<img src=assets/' +images[i] + ' "> ';
-document.getElementsByClassName("options")[i].setAttributes("id", title[i]);
-document.getElementsByClassName("options")[i].addEventListener("click", function(){alert("congrats you clicked a div")});
-}
-console.log(sectionID);
-document.getElementByID(sectionID).classlist.toggle("blue");
+  document.getElementsByClassName("options")[i].innerHTML += '<img src= "assets/' + images[i] + ' "> ';
