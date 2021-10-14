778d6d81f287540eeb55ddb8d419375660bf9d26
elianasweet <elsw4258@colorado.edu>
2021-10-14 12:12:59 -0600

    push

--- week8/index.js
+++ week8/index.js
@@ -3,12 +3,13 @@
 console.log( document.getElementsByClassName("options") );
 
 
-const images = ["moon.jpeg", "sun,jpg", "stars.jpeg"];
+const images = ["moon.jpeg", "sun.jpg", "stars.jpeg"];
 const title = ["moon", "sun", "stars"];
 
 
 for(i=0;i<document.getElementsByClassName('options').length; i++){
 
-  document.getElementByClassName('label')[i].innerHTML = title[i];
+  document.getElementsByClassName('label')[i].innerHTML = title[i];
 
   document.getElementsByClassName("options")[i].innerHTML += '<img src= "assets/' + images[i] + ' "> ';
+}
