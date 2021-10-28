f27357e422f9956831c0c7393a1e11041e6c9f22
elianasweet <elsw4258@colorado.edu>
2021-10-28 12:10:24 -0600

    midterm 2

--- midterm/index.js
+++ midterm/index.js
@@ -2,6 +2,33 @@ console.log( document.getElementsByClassName("options") );
 
 const images = ["monday.jpeg","tuesday.jpg","wednesday.jpeg","thursday.jpeg","friday.jpeg","saturday.jpeg","sunday.jpeg"];
 const title = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+const quotes = ["The best preperation for tomorrow is doing your best today.","Happiness is a direction, not a place.", "Live life to the fullest, and focus on the positive.","Life is 10% what happens to you and 90% how you react to it.","Set your goals high, and don't stop till you get there."];
+
+let quotecount = 0;
+document.getElementById("quotebutton").addEventListener("click",buttonClicked);
+  //document.getElementById("quotecontainer").classList.toggle("shown");;
+
+
+function buttonClicked(){
+  console.log("clickedquote");
+  document.getElementById("qtext").innerHTML = quotes[quotecount];
+  if(quotecount>= quotes.length-1){
+    quotecount = 0;
+  }else{
+    quotecount++;
+
+}
+}
+
+
+
+
+
+
+
+
+
+
 for(i=0;i<document.getElementsByClassName('options').length; i++){
   document.getElementsByClassName('label')[i].innerHTML = title[i];
   document.getElementsByClassName("options")[i].innerHTML += '<img src= "assets/' + images[i] + ' "> ';
