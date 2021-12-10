5bfab94bc8dca8b2c0b973d7d2a2f773f681624f
elianasweet <elsw4258@colorado.edu>
2021-12-10 14:20:10 -0700

    final

--- wk 11 actually/index.js
+++ wk 11 actually/index.js
@@ -1,19 +1,19 @@
-console.log('hello!');
+console.log( document.getElementsByClassName("backgrond") );
 
-let pics = 8;
-let pic;
+const quotes = ["The best preperation for tomorrow is doing your best today.","Happiness is a direction, not a place.", "Live life to the fullest, and focus on the positive.","Life is 10% what happens to you and 90% how you react to it.","Set your goals high, and don't stop till you get there."];
 
-for(i=0;i<pics;i++){
-  pic = 1+i;
-document.getElementById('portfolio').innerHTML += <"div class = 'pimage'> <img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' />";
+let quotecount = 0;
+document.getElementById("quotebutton").addEventListener("click",buttonClicked);
+  //document.getElementById("quotecontainer").classList.toggle("shown");;
+
+
+function buttonClicked(){
+  console.log("clickedquote");
+  document.getElementById("qtext").innerHTML = quotes[quotecount];
+  if(quotecount>= quotes.length-1){
+    quotecount = 0;
+  }else{
+    quotecount++;
+
 }
-let displaydetails = false;
- document.getElementById('portfolio').addEventListener("click",
-showDetails);
-
-function showDetails(){
-  document.getElementById('details').style.display = "block";
-  document.getElementById('detailsoverlay').style.display = "block";
-  document.getElementById('detailsoverlay').addEventListener = ()"click", clickedoverlay);
-  displaydetails = true;
 }
